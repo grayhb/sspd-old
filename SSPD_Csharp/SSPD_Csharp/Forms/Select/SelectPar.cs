81f05dbac466ae985e7e1391251d427d6e8c8de5
ShadrovSV <ShadrovSV@92984c1e-d43d-1a91-fd17-2681e60a06b9>
2012-08-15 06:28:33 +0000

    Форма выбора параметра

---
+++ SSPD_Csharp/SSPD_Csharp/Forms/Select/SelectPar.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace SSPD.Select
+{
+    public partial class SelectPar : Form
+    {
+
+        //возврат значения
+        public bool FlSel = false;
+
+        public int IDSel;
+        public Dictionary<int, string> SetPar;
+
+        public SelectPar(string FormText, string SerParLabel)
+        {
+            InitializeComponent();
+            
+            if (FormText != null)
+                this.Text = FormText;
+            else
+                this.ControlBox = false;
+
+            if (SerParLabel != null) ParLabel.Text = SerParLabel;
+        }
+
+        private void SelectPar_Load(object sender, EventArgs e)
+        {
+            SSPDUI.ComboType CT = new SSPDUI.ComboType();
+
+            foreach (int IDPar in SetPar.Keys)
+            {
+                CT.ID_Item = IDPar;
+                CT.Name_Item = SetPar[IDPar];
+                Par.Items.Add(CT);
+                if (IDPar == IDSel)
+                    Par.SelectedItem = Par.Items[Par.Items.Count - 1];
+            }
+
+            Par.Focus();
+        }
+
+        #region [События элементов формы]
+
+        private void SelectPar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                FlSel = true;
+                this.Close();
+            }
+            else if (e.KeyCode == Keys.Escape)
+                this.Close();
+        }
+        
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FlSel = true;
+            this.Close();
+        }
+
+        #endregion
+
+
+
+
+    }
+}
